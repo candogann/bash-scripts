2301aae9eadcf8ad2a50b0e8f98125f6a309e497
Ali Can Doğan <candogann01@gmail.com>
2022-12-03 23:41:45 +0300

    Initial commit

---
+++ login.sh
@@ -0,0 +1,1 @@
+sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-central-1) 581644921650.dkr.ecr.eu-central-1.amazonaws.com
---
+++ push.sh
@@ -0,0 +1,11 @@
+set -e
+set -x
+
+IMAGE_TAG=$1
+BUILD_PATH=$2
+ACC_ID=581644921650
+
+
+sudo docker build $BUILD_PATH --tag $IMAGE_TAG
+sudo docker tag $IMAGE_TAG $ACC_ID.dkr.ecr.eu-central-1.amazonaws.com/$IMAGE_TAG
+sudo docker push $ACC_ID.dkr.ecr.eu-central-1.amazonaws.com/$IMAGETAG
