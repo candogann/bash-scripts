541e8af35c14b9dd749b62a0e5ba3e8638ff48bf
candogann <101297290+candogann@users.noreply.github.com>
2022-12-03 23:58:07 +0300

    Update login.sh

--- ECR Push/login.sh
+++ ECR Push/login.sh
@@ -1,1 +1,2 @@
-sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-central-1) 581644921650.dkr.ecr.eu-central-1.amazonaws.com
+ACC_ID=000000000000 # change this here
+sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-central-1) $ACC_ID.dkr.ecr.eu-central-1.amazonaws.com
